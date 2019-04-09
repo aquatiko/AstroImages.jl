1308efa2b23a674c7233296d9e153596e98ef044
Rohit Kumar <rohit970819@gmail.com>
2019-04-09 19:05:47 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,51 +3,6 @@ using Test
 
 import AstroImages: _float, render
 
-using AstroImages, FITSIO, Images, Random
-using Test
-
-import AstroImages: _float, render
-fname = tempname() * ".fits"
-@testset "default handler" begin
-        @testset "less dimensions than 2" begin
-            data = rand(2)
-            FITS(fname, "w") do f4
-                write(f4, data)
-            end
-            @test_throws ErrorException AstroImage(fname)
-        end
-
-        @testset "no ImageHDU" begin
-            ## Binary table
-            indata = Dict{String, Array}()
-            i = length(indata) + 1
-            indata["col$i"] = [randstring(10) for j=1:20]  # ASCIIString column
-            i += 1
-            indata["col$i"] = ones(Bool, 20)  # Bool column
-            i += 1
-            indata["col$i"] = reshape([1:40;], (2, 20))  # vector Int64 column
-            i += 1
-            indata["col$i"] = [randstring(5) for j=1:2, k=1:20]  # vector ASCIIString col
-            indata["vcol"] = [randstring(j) for j=1:20]  # variable length column
-            indata["VCOL"] = [collect(1.:j) for j=1.:20.] # variable length
-
-            FITS(fname, "w") do f4
-                write(f4, indata; varcols=["vcol", "VCOL"])
-                @test_throws MethodError AstroImage(f4)
-            end
-        end
-        
-        @testset "AstroImage file opening ways" begin
-                data = rand(2,2)
-                FITS(fname, "w") do f4
-                    write(f4, data)
-                end
-            @test AstroImage(fname, 1) isa AstroImage
-                @test AstroImage(Gray ,fname, 1) isa AstroImage
-       end
-end
-
-
 @testset "Conversion to float and fixed-point" begin
     @testset "Float" begin
         for T in (Float16, Float32, Float64)
@@ -72,17 +27,64 @@ end
     end
 end
 
-@testset "FITS and images 1" begin
-        for T in [UInt8, Int8, UInt16, Int16, UInt32, Int32, Int64,
-                  Float32, Float64]
-            data = reshape(T[1:100;], 5, 20)
-            FITS(fname, "w") do f1
-                write(f1, data)
-            end
-            @test load(fname) == data
-            @test load(fname, (1, 1)) == (data, data)
-            img = AstroImage(fname)
-            rendered_img = render(img)
-            @test iszero(minimum(rendered_img))
+@testset "FITS and images" begin
+    fname = tempname() * ".fits"
+    for T in [UInt8, Int8, UInt16, Int16, UInt32, Int32, Int64,
+              Float32, Float64]
+        data = reshape(T[1:100;], 5, 20)
+        FITS(fname, "w") do f
+            write(f, data)
+        end
+        @test load(fname) == data
+        @test load(fname, (1, 1)) == (data, data)
+        img = AstroImage(fname)
+        rendered_img = render(img)
+        @test iszero(minimum(rendered_img))
+        @test convert(Matrix{Gray}, img) == rendered_img
     end
+    rm(fname, force=true)
 end
+
+
+@testset "default handler" begin
+    fname = tempname() * ".fits"
+    @testset "less dimensions than 2" begin
+        data = rand(2)
+        FITS(fname, "w") do f
+            write(f, data)
+        end
+        @test_throws ErrorException AstroImage(fname1)
+    end
+
+    @testset "no ImageHDU" begin
+        ## Binary table
+        indata = Dict{String, Array}()
+        i = length(indata) + 1
+        indata["col$i"] = [randstring(10) for j=1:20]  # ASCIIString column
+        i += 1
+        indata["col$i"] = ones(Bool, 20)  # Bool column
+        i += 1
+        indata["col$i"] = reshape([1:40;], (2, 20))  # vector Int64 column
+        i += 1
+        indata["col$i"] = [randstring(5) for j=1:2, k=1:20]  # vector ASCIIString col
+        indata["vcol"] = [randstring(j) for j=1:20]  # variable length column
+        indata["VCOL"] = [collect(1.:j) for j=1.:20.] # variable length
+
+        FITS(fname, "w") do f
+            write(f, indata; varcols=["vcol", "VCOL"])
+            @test_throws MethodError AstroImage(f)
+        end
+    end
+        
+    @testset "AstroImage file opening ways" begin
+                data = rand(2,2)
+                FITS(fname, "w") do f4
+                    write(f4, data)
+                end
+            @test AstroImage(fname, 1) isa AstroImage
+                @test AstroImage(Gray ,fname, 1) isa AstroImage
+       end
+        
+    rm(fname, force = true)
+end
+include("plots.jl")
